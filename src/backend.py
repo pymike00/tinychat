9a4be5d2a324316581e4fed09a35bf5d5b004832
pymike00 <pymike00@gmail.com>
2023-12-25 20:12:31 +0100

    REFACTOR: Backend.models is now _protected

--- src/backend.py
+++ src/backend.py
@@ -4,19 +4,19 @@ from llms import CohereChat, GPT35Turbo, LLMProtocol
 class Backend:
     def __init__(self) -> None:
         self._llm: LLMProtocol = GPT35Turbo()
-        self.models = {
+        self._models = {
             "GPT-3.5 Turbo": GPT35Turbo,
             "Cohere Chat": CohereChat
             # Add other models as needed
         }
 
     def available_models(self) -> list:
-        return list(self.models.keys())
+        return list(self._models.keys())
     
     def set_model(self, model_name: str) -> None:
         if model_name not in self.available_models():
             raise KeyError("Invalid Model Name")
-        self._llm = self.models[model_name]()
+        self._llm = self._models[model_name]()
 
     def get_chat_response(self, user_input: str) -> str:
         return self._llm.get_response(user_input)
