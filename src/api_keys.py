93266b9197b308a598785bb503abe52a6f2d2b9f
pymike00 <pymike00@gmail.com>
2023-12-31 12:46:46 +0100

    UPDATE: improve api_keys functions to add type hinting

--- src/api_keys.py
+++ src/api_keys.py
@@ -4,24 +4,24 @@ import json
 
 from settings import SECRETS_FILE_PATH
 
-def load_api_keys(file_path):
+
+def load_api_keys(file_path: str) -> dict:
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             return json.load(file)
     except FileNotFoundError:
-        with open(file_path, 'w') as file:
+        with open(file_path, "w") as file:
             json.dump({}, file)
-        return {}        
+        return {}
 
-def set_api_key(key, value):
+
+def set_api_key(key: str, value: str) -> None:
     secrets = load_api_keys(SECRETS_FILE_PATH)
     secrets[key] = value
-    with open(SECRETS_FILE_PATH, 'w') as file:
+    with open(SECRETS_FILE_PATH, "w") as file:
         json.dump(secrets, file, indent=4)  # Writing back the updated secrets
 
-def get_api_key(key):
+
+def get_api_key(key: str) -> str:
     secrets = load_api_keys(SECRETS_FILE_PATH)
     return secrets.get(key, "")
-
-
-
