2925ec15b67b4f13e223ef9fafecad0bb34b84a8
pymike00 <pymike00@gmail.com>
2024-01-02 22:17:36 +0100

    REFACTOR: use post requests in CohereClientWrapper to remove client dependency

--- src/llms/cohere_wrap.py
+++ src/llms/cohere_wrap.py
@@ -1,23 +1,46 @@
-from cohere import Client as CohereClient
+import requests
 
-from llms.base import BaseLLMWrapper
+from llms.base import BaseLLMClient
 from settings import COHERE_API_KEY_NAME
 
 
-class CohereClientWrapper(BaseLLMWrapper):
+class CohereClientWrapper(BaseLLMClient):
     """
     Cohere chat client.
     """
 
+    COHERE_CHAT_API_URL = "https://api.cohere.ai/v1/chat"
+
     def __init__(self) -> None:
-        super().__init__(client=CohereClient, api_key_name=COHERE_API_KEY_NAME)
+        super().__init__(api_key_name=COHERE_API_KEY_NAME)
         self.chat_history = []
 
-    def get_response(self, user_input: str) -> str:
-        response = self.client.chat(
-            chat_history=self.chat_history, message=user_input, temperature=1
-        )
-        answer = response.text
+    def _update_chat_history(self, user_input: str, answer: str) -> None:
         self.chat_history.append({"role": "User", "message": user_input})
         self.chat_history.append({"role": "Chatbot", "message": answer})
-        return answer
+
+    def _make_api_request(self, user_input: str) -> None:
+        data = {
+            "chat_history": self.chat_history,
+            "message": user_input,
+            "temperature": 1.0,
+        }
+        response = requests.post(
+            self.COHERE_CHAT_API_URL,
+            headers=self.default_headers(),
+            json=data,
+        )
+        if response.status_code != 200:
+            raise ValueError(f"Server responded with error: {response.status_code}")
+        try:
+            return response.json().get("text", "No response text found")
+        except ValueError:
+            raise ValueError("Invalid response format received from server.")
+
+    def get_response(self, user_input: str) -> str:
+        try:
+            chat_response = self._make_api_request(user_input=user_input)
+        except ValueError as e:
+            return e
+        self._update_chat_history(user_input, chat_response)
+        return chat_response
