b03d6887b7243c9c065457006022aa0a01aba37d
pymike00 <pymike00@gmail.com>
2024-01-03 16:22:43 +0100

    ADD: tests for BaseLLMClient

---
+++ tests/test_llms_base.py
@@ -0,0 +1,34 @@
+import unittest
+from unittest.mock import patch
+
+from tinychat.llms.base import BaseLLMClient
+from tinychat.settings import SECRETS_FILE_PATH
+
+
+class TestBaseLLMClient(unittest.TestCase):
+
+    @patch('tinychat.llms.base.get_api_key')
+    def test_api_key_setter_valid_key(self, mock_get_api_key):
+        mock_get_api_key.return_value = 'test_api_key'
+        client = BaseLLMClient(api_key_name='test_api_key_name')
+        self.assertEqual(client.api_key, 'test_api_key')
+
+    @patch('tinychat.llms.base.get_api_key')
+    def test_api_key_setter_invalid_key(self, mock_get_api_key):
+        mock_get_api_key.return_value = None
+        with self.assertRaises(ValueError) as context:
+            BaseLLMClient(api_key_name='invalid_api_key_name')
+        self.assertIn('invalid_api_key_name was not found in', str(context.exception))
+        self.assertIn(SECRETS_FILE_PATH, str(context.exception))
+
+    @patch('tinychat.llms.base.get_api_key')
+    def test_default_headers(self, mock_get_api_key):
+        mock_get_api_key.return_value = 'some-api-key'
+        client = BaseLLMClient(api_key_name='test_api_key_name')
+        expected_headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": "Bearer some-api-key",
+        }
+        self.assertEqual(client.default_headers(), expected_headers)
+
