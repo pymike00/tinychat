c59a423413728e18afe06272cc49e90964096630
pymike00 <pymike00@gmail.com>
2024-01-06 11:59:45 +0100

    FIX: wrong imports

--- tests/llms/test_google_handler.py
+++ tests/llms/test_google_handler.py
@@ -1,16 +1,16 @@
 import unittest
 from unittest.mock import patch
 
-from tinychat.llms.google import GoogleGeminiHandler, GoogleGeminiClient
+from tinychat.llms.google import GoogleAIHandler, GoogleAIClient
 
 
 class TestGoogleGeminiHandler(unittest.TestCase):
-    @patch.object(GoogleGeminiClient, "perform_chat_request")
+    @patch.object(GoogleAIClient, "perform_chat_request")
     def test_get_response(self, mock_perform_chat_request):
         test_user_input = "hello Bard"
         test_model_response = "hello dear user"
         mock_perform_chat_request.return_value = test_model_response
-        handler = GoogleGeminiHandler()
+        handler = GoogleAIHandler()
         response = handler.get_response(test_user_input)
         self.assertEqual(response, test_model_response)
         expected_messages = [
