a3698ad15ef5d00af84de2049a845eef1c9515a6
pymike00 <pymike00@gmail.com>
2024-01-08 21:18:35 +0100

    UPDATE: MistralClient & MistralHandler can now handle response streaming via SSE

--- tinychat/llms/mistral.py
+++ tinychat/llms/mistral.py
@@ -1,4 +1,8 @@
+import json
+from typing import Generator
+
 import requests
+from sseclient import SSEClient
 
 from tinychat.llms.base import BaseLLMClient
 from tinychat.settings import MISTRAL_API_KEY_NAME
@@ -24,7 +28,7 @@ class MistralClient(BaseLLMClient):
         }
         response = requests.post(
             self.MISTRAL_COMPLETION_API_URL,
-            headers=self.default_headers(),
+            headers=self.default_headers(),  # type: ignore
             json=data,
         )
         if response.status_code != 200:
@@ -35,6 +39,26 @@ class MistralClient(BaseLLMClient):
             return response.json()["choices"][0]["message"]["content"]
         except KeyError as e:
             raise KeyError(f"Invalid response format received from server. {e}")
+
+    def perform_stream_request(self, messages: list[dict]) -> SSEClient:
+        data = {
+            "model": self.model_name,
+            "messages": messages,
+            "temperature": self.temperature,
+            "stream": True,
+        }
+        response = requests.post(
+            self.MISTRAL_COMPLETION_API_URL,
+            headers=self.default_headers(),  # type: ignore
+            json=data,
+            stream=True,
+        )
+        if response.status_code != 200:
+            print(response.content)
+            raise ValueError(
+                f"Server responded with an error. Status Code: {response.status_code}"
+            )
+        return SSEClient(event_source=response)  # type: ignore
 
 
 class MistralHandler:
@@ -52,7 +76,32 @@ class MistralHandler:
         self._client = MistralClient(model_name=model_name)
 
     def get_response(self, user_input: str) -> str:
+        """Return complete chat response from client"""
         self._messages.append({"role": "user", "content": user_input})
         chat_response = self._client.perform_chat_request(self._messages)
         self._messages.append({"role": "assistant", "content": chat_response})
         return chat_response
+
+    def stream_response(self, user_input: str) -> Generator[str, None, None]:
+        """
+        Yield stream responses from the client as they are received.
+
+        This method sends the user input to the client and then yields each piece
+        of the language model's response as it is received in real-time. After the
+        streaming is complete, it updates the message list with the user input and
+        the full language model response.
+
+        :param user_input: The input string from the user to be sent to the model.
+        :return: A generator yielding the model's response in streamed parts.
+        """
+        self._messages.append({"role": "user", "content": user_input})
+        stream = self._client.perform_stream_request(self._messages)
+        lm_response = ""
+        for event in stream.events():  # type: ignore
+            if event.data != "[DONE]":
+                json_load = json.loads(event.data)["choices"][0]["delta"]
+                if "content" in json_load.keys():
+                    response_piece = json_load["content"]
+                    lm_response += response_piece
+                    yield response_piece
+        self._messages.append({"role": "assistant", "content": lm_response})
