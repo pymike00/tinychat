9bf656ec63c3ceae079c045f0c74f4fe1068e9f1
pymike00 <pymike00@gmail.com>
2024-01-09 21:12:45 +0100

    ADD: TestGoogleGeminiHandlerStreaming

--- tests/llms/test_google_handler.py
+++ tests/llms/test_google_handler.py
@@ -1,5 +1,6 @@
+import json
 import unittest
-from unittest.mock import patch
+from unittest.mock import MagicMock, Mock, patch
 
 from tinychat.llms.google import GoogleAIHandler, GoogleAIClient
 
@@ -19,5 +20,52 @@ class TestGoogleGeminiHandler(unittest.TestCase):
                 "parts": [{"text": test_model_response}],
                 "role": "model",
             },
+        ]
+        self.assertEqual(handler._messages, expected_messages)
+
+
+class TestGoogleGeminiHandlerStreaming(unittest.TestCase):
+    @patch.object(GoogleAIClient, "perform_stream_request")
+    def test_stream_response(self, mock_perform_stream_request):
+        # Create a mock SSEClient with a mock events method
+        mock_sse_client = MagicMock()
+        mock_stream = iter(
+            [
+                Mock(
+                    data=json.dumps(
+                        {
+                            "candidates": [
+                                {"content": {"parts": [{"text": "response part 1"}]}}
+                            ]
+                        }
+                    )
+                ),
+                Mock(
+                    data=json.dumps(
+                        {
+                            "candidates": [
+                                {"content": {"parts": [{"text": "response part 2"}]}}
+                            ]
+                        }
+                    )
+                ),
+                Mock(data="[DONE]"),
+            ]
+        )
+        mock_sse_client.events.return_value = mock_stream
+        mock_perform_stream_request.return_value = mock_sse_client
+
+        handler = GoogleAIHandler()
+        generator = handler.stream_response("hello")
+
+        # Extracting and verifying the stream response
+        responses = []
+        for part in generator:
+            responses.append(part)
+
+        self.assertEqual(responses, ["response part 1", "response part 2"])
+        expected_messages = [
+            {"parts": [{"text": "hello"}], "role": "user"},
+            {"parts": [{"text": "response part 1response part 2"}], "role": "model"},
         ]
         self.assertEqual(handler._messages, expected_messages)
