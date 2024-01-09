25a4686929d0fda24024992e2f83a17183fd0254
pymike00 <pymike00@gmail.com>
2024-01-09 21:29:27 +0100

    ADD: TestGoogleAIClientStreaming

--- tests/llms/test_google_client.py
+++ tests/llms/test_google_client.py
@@ -1,5 +1,6 @@
+import json
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import MagicMock, Mock, patch
 
 from requests.models import Response
 
@@ -49,4 +50,62 @@ class TestGoogleAIClient(unittest.TestCase):
             client.perform_chat_request(messages)
         self.assertIn(
             "Invalid response format received from server.", str(context.exception)
+        )
+
+
+class TestGoogleAIClientStreaming(unittest.TestCase):
+    @patch("tinychat.llms.google.requests.post")
+    @patch("tinychat.llms.google.SSEClient")
+    def test_perform_stream_request_success(self, mock_sse_client, mock_post):
+        # Mocking SSEClient and the response
+        mock_response = Mock(spec=Response)
+        mock_response.status_code = 200
+        mock_post.return_value = mock_response
+
+        # Creating mock events
+        mock_event1 = MagicMock()
+        mock_event1.data = json.dumps(
+            {"candidates": [{"content": {"parts": [{"text": "part1"}]}}]}
+        )
+        mock_event2 = MagicMock()
+        mock_event2.data = json.dumps(
+            {"candidates": [{"content": {"parts": [{"text": "part2"}]}}]}
+        )
+        mock_event_done = MagicMock()
+        mock_event_done.data = "[DONE]"
+
+        # Setting up a test stream
+        test_stream = [mock_event1, mock_event2, mock_event_done]
+        mock_sse_client.return_value.events = MagicMock(return_value=iter(test_stream))
+
+        client = GoogleAIClient()
+        messages = [{"parts": [{"text": "content"}], "role": "user"}]
+        stream = client.perform_stream_request(messages)
+
+        # Extracting and verifying the stream response
+        responses = []
+        for event in stream.events():
+            if event.data != "[DONE]":
+                event_data = json.loads(event.data)
+                response_piece = event_data["candidates"][0]["content"]["parts"][0][
+                    "text"
+                ]
+                responses.append(response_piece)
+
+        self.assertEqual(responses, ["part1", "part2"])
+
+    @patch("tinychat.llms.google.requests.post")
+    def test_perform_stream_request_failure(self, mock_post):
+        # Mocking the response with an error status code
+        mock_response = Mock(spec=Response)
+        mock_response.status_code = 400
+        mock_post.return_value = mock_response
+
+        client = GoogleAIClient()
+        messages = [{"parts": [{"text": "content"}], "role": "user"}]
+
+        with self.assertRaises(ValueError) as context:
+            next(client.perform_stream_request(messages))  # type: ignore
+        self.assertIn(
+            "Server responded with an error. Status Code: 400", str(context.exception)
         )
