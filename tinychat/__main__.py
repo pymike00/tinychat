a51788fb70b755aadfaf91208a59804c89e98a85
pymike00 <pymike00@gmail.com>
2024-01-12 12:58:58 +0100

    UPDATE: set appearance mode as dark (ui)

--- tinychat/__main__.py
+++ tinychat/__main__.py
@@ -4,6 +4,7 @@ from tinychat.backend import Backend
 from tinychat.ui.chat import ChatApp
 
 customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_appearance_mode("dark")
 
 
 
